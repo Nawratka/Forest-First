a7caf422d7ea3d7505cadac2ed6ab4c6fef6d415
Kamila <kamilanawrat@gmail.com>
2024-07-02 19:22:04 +0200

    services rwd - gap

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -84,26 +84,32 @@ const scrollSpySections = document.querySelectorAll('.scroll-section');
 const menuItems = document.querySelectorAll('.nav__list-link');
 
 const handleScrollSpy = () => {
-	const sections = [];
-	scrollSpySections.forEach((section) => {
-		if (window.scrollY <= section.offsetTop + section.offsetHeight - 103) {
-			if (section.id === 'services') {
-				if (window.scrollY > 600) {
-					document
-						.querySelectorAll('.service-box')
-						.forEach((box) => box.classList.add('showing-icons'));
-				}
-				return;
+	{
+		
+		const sections = [];
+		scrollSpySections.forEach((section) => {
+			
+			if (window.scrollY <= section.offsetTop + section.offsetHeight - 103) {
+				if (section.id === 'services') {
+					if (window.scrollY > 600) {
+						document
+							.querySelectorAll('.service-box')
+							.forEach((box) => box.classList.add('showing-icons'));
+					}
+					return;}
+				sections.push(section.id);
+
+				const activeSection = document.querySelector(
+					`[href*="${sections[0]}"]`
+				);
+
+				menuItems.forEach((item) => item.classList.remove('active'));
+
+				activeSection.classList.add('active');
 			}
-			sections.push(section.id);
-
-			const activeSection = document.querySelector(`[href*="${sections[0]}"]`);
-
-			menuItems.forEach((item) => item.classList.remove('active'));
-
-			activeSection.classList.add('active');
-		}
-	});
+		});
+		
+	}
 };
 
 window.addEventListener('scroll', handleScrollSpy);
